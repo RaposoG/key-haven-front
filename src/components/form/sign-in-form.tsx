262dac88f58fb4871c8920f888c915d5d2a9fba3
raposoG <gabriel.henrique7@hotmail.com>
2025-03-18 17:14:47 -0300

    feat(theme): adição do provedor de tema e toggle
    - Criado o componente ThemeProvider para gerenciar o tema da aplicação.
    - Implementado o hook useTheme para facilitar o acesso ao contexto do tema.
    - Adicionado o componente ThemeToggle para permitir a troca entre temas (claro, escuro e sistema).
    - Modificado o arquivo main.tsx para envolver o RouterProvider com o ThemeProvider.
    - Alterado o estilo do texto no componente SignInForm.
    
    Essas alterações introduzem um sistema de gerenciamento de temas que melhora a experiência do usuário, permitindo que ele escolha entre diferentes modos de visualização, além de garantir que a aplicação se adapte às preferências do sistema do usuário.

--- src/components/form/sign-in-form.tsx
+++ src/components/form/sign-in-form.tsx
@@ -49,7 +49,7 @@ export function SignInForm({ className, ...props }: React.ComponentPropsWithoutR
               </div>
 
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                <span className="relative z-10 bg-background px-2 text-muted-foreground">Or continue with</span>
+                <span className="relative z-10 bg-card px-2 text-muted-foreground">Or continue with</span>
               </div>
 
               <div className="flex flex-col gap-4">
