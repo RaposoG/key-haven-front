45aa03121cfa04c70244a5b30dcac2eace4ddd61
luizpbello <luizpaulopinto25@gmail.com>
2025-03-19 15:05:53 -0300

    feat(auth): implement logout functionality and protect dashboard route

--- src/routes/app/dashboard/route.tsx
+++ src/routes/app/dashboard/route.tsx
@@ -1,7 +1,20 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
+/**
+ * TODO: Tipagem não está pegando mas funciona.
+ * @see https://tanstack.com/router/v1/docs/framework/react/guide/authenticated-routes#authentication-using-react-contexthooks
+ * @see main.tsx
+ */
 export const Route = createFileRoute("/app/dashboard")({
   component: RouteComponent,
+  beforeLoad: ({ context, location }) => {
+    if (!context.auth.isAuthenticated) {
+      throw redirect({
+        to: "/",
+        search: { redirect: location.href },
+      });
+    }
+  },
 });
 
 function RouteComponent() {
