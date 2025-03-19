408b2b52b83384a874d03c1a52bb88acf164fdff
raposoG <gabriel.henrique7@hotmail.com>
2025-03-19 06:31:03 -0300

    refatoração(routes): adicionar novas rotas de autenticação, configurações e armazenamento
    - Atualizado o arquivo app-sidebar.tsx para incluir novos itens de navegação: "Dashboard", "Authentication", "Storage" e "Settings".
    - Modificado o nav-main.tsx para utilizar links de navegação em vez de colapsáveis.
    - Criadas novas rotas para "/app/authentication", "/app/settings" e "/app/storage" no arquivo routeTree.gen.ts.
    - Implementados novos componentes de rota para autenticação, configurações e armazenamento, cada um retornando uma mensagem simples.
    
    Essas alterações melhoram a estrutura de navegação do aplicativo, facilitando o acesso às novas funcionalidades e aprimorando a experiência do usuário ao navegar entre as seções do aplicativo.

--- src/components/sidebar/nav-user.tsx
+++ src/components/sidebar/nav-user.tsx
@@ -17,9 +17,9 @@ export function NavUser({
 
   return (
     <SidebarMenu>
-      <SidebarMenuItem>
+      <SidebarMenuItem className="flex flex-col">
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
+          <DropdownMenuTrigger>
             <SidebarMenuButton size="lg" className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.avatar} alt={user.name} />
